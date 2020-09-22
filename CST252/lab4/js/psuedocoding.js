dc5ff102a92ec1486e48de9ead60f7f4dacc43d3
hpaige6973 <65097496+hpaige6973@users.noreply.github.com>
2020-09-22 12:16:56 -0700

    added up to lab6

---
+++ CST252/lab4/js/psuedocoding.js
@@ -0,0 +1,5 @@
+// Reload map before each round after dying
+    // Move up down left or right
+    // Eat up one dot
+// Evaluate is you touched a ghost
+// Play end over screen or repeat 2-4
---
+++ CST252/lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Hannah Piage
+ * Created:   09.14.2020
+ * 
+ * (c) Copyright by me Corp.
+ **/
+
+ // Define Variables
+ make="Hyundai";
+ model="Accent";
+ color="white";
+ year=2017;
+
+ //Calculate
+ age=2020-2017;
+
+ //output
+ document.writeIn("make: " + make + "<br>");
+ document.writeIn("model: " + model + "<br>");
+ document.writeIn("color: " + color + "<br>");
+ document.writeIn("year: " + year + "<br>");
+ document.writeIn("age: " + age + " years<br>");

9b818762e0ea72cbd3810ae5c5a695405964b88e
hpaige6973 <65097496+hpaige6973@users.noreply.github.com>
2020-09-22 19:15:18 -0700

    Add files via upload

---
+++ lab6/js/lab6.js
@@ -0,0 +1,25 @@
+/**
+ * Author:    Hannah Paige
+ * Created:   09.16.2020
+ * 
+ * (c) Copyright by Blub Corp.
+ **/
+
+ //Define Variables
+ myTransport = ["MST", "car","boyfriend's car"];
+
+ myMainRide = {
+ 	make: "Hyundai"
+ 	model: "Accent"
+ 	color: "white"
+ 	year: "2017"
+ 	age: function() {
+ 		return 2020-age;
+ 	}
+ }
+
+ //output
+ document.writeln("My Main Ride:", myMainRide, "</br>");
+ document.writeln("My Transportation:", myTransport, "</br>");
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
---
+++ lab7/js/lab7.js
@@ -0,0 +1,16 @@
+/**
+ * Author:    Hannah Paige
+ * Created:   09.22.2020
+ * 
+ * (c) Copyright by Blub Corp.
+ **/
+
+ /**
+ * Multiplies two numbers
+ * @param {Number} x
+ * @param {Number} y
+ * @return {Number} product
+ */
+ function multiply(x,y) {
+ 	return x * y;
+ }
